f9f876e57964e789459739eebce7f1666e22fded
Zambom <erikzambom@hotmail.com>
2016-08-16 14:50:59 -0300

    Lista de participantes

--- subscriptions/context_processors.py
+++ subscriptions/context_processors.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from .models import Subscribe
 
+@login_required
 def subscribed_courses(request):
 	return {
-		'subscribed_courses': Subscribe.objects.filter(user = request.user)[:3]
+		'subscribed_courses': Subscribe.objects.filter(user = request.user)[:3] or None
 	}
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -5,8 +5,10 @@ from django.core.paginator import Paginator, EmptyPage
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.translation import ugettext_lazy as _
+from rolepermissions.mixins import HasRoleMixin
 from .forms import SubscribeForm
 from .models import Subscribe
+from courses.models import Course
 
 @login_required
 def subscribe(request):
@@ -23,8 +25,9 @@ def subscribe(request):
 
 			return redirect('app:course:manage')
 
-class Index(LoginRequiredMixin, generic.ListView):
+class Index(HasRoleMixin, LoginRequiredMixin, generic.ListView):
 
+	allowed_roles = ['student']
 	login_url = '/'
 	redirect_field_name = 'next'
 	template_name = 'subscribed/index.html'
@@ -33,3 +36,16 @@ class Index(LoginRequiredMixin, generic.ListView):
 
 	def get_queryset(self):
 		return Subscribe.objects.filter(user = self.request.user)
+
+class Participants(HasRoleMixin, LoginRequiredMixin, generic.ListView):
+
+	allowed_roles = ['professor', 'system_admin']
+	login_url = '/'
+	redirect_field_name = 'next'
+	template_name = 'subscribed/participants.html'
+	context_object_name = 'subscribers'
+	paginate_by = 10
+
+	def get_queryset(self):
+		course = get_object_or_404(Course, slug = self.kwargs.get('slug'))
+		return Subscribe.objects.filter(course = course)
